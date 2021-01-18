eb782bbfe514fe465e0e2bdf72a58648321b3715
mohsinabbas095 <74142640+mohsinabbas095@users.noreply.github.com>
2021-01-18 07:39:21 +0500

    I'm changing the initial screen to Location screen now.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:clima/screens/city_screen.dart';
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
 
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: CityScreen(),
+      home: LocationScreen(),
     );
   }
 }
